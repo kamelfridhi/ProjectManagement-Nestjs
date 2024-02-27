d13bbb8d986d9333e239141c919c7ae0afc39656
faroukBenJeddou <farouk.benjeddou@esprit.tn>
2024-02-27 23:09:44 +0100

    fixation

---
+++ src/modules/team-module/dto/createTeam.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateTeamDto{
+@IsNotEmpty()
+@IsString()
+  name:string;
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+}
--- src/modules/team-module/team.service.ts
+++ src/modules/team-module/team.service.ts
@@ -3,6 +3,41 @@ https://docs.nestjs.com/providers#services
 */
 
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { BaseService } from 'src/global-utils/base.service';
+import { Team } from 'src/schemas/team.schema';
+import { CreateTeamDto } from "./dto/createTeam.dto";
+import { UpdateTaskDto } from "../task/dto/updateTask.dto";
+import { UpdateTeamDto } from "./dto/updateTeam.dto";
 
 @Injectable()
-export class TeamService { }
+export class TeamService extends BaseService<Team> {
+
+    constructor(
+       
+        @InjectModel(Team.name) private TeamModel: Model<Team>,
+    ) {
+        super(TeamModel);
+    }
+    async createTeam(createTaskDto: CreateTeamDto) {
+        return super.create(createTaskDto)
+    }
+
+    getAllTeams() {
+        return super.findAll();
+     }
+ 
+     getOneTeam(id: string) {
+         return super.findOne(id);
+     }
+ 
+     updateTeam(id: string, updateTeamDto: UpdateTeamDto) {
+         return super.update(id,updateTeamDto);
+     }
+ 
+     deleteTeam(id: string) {
+         return super.remove(id);
+     }
+ 
+ }
