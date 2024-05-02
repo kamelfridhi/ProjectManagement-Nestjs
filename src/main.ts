d3e3136d2d02c0e3e6958e3e4564feeab4186115
kamel.fridhi <80178238+kamelfridhi@users.noreply.github.com>
2024-05-02 22:49:22 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // Allow requests from this origin
+    origin: 'http://192.168.1.16:5173', // Allow requests from this origin
     credentials: true, // Allow sending cookies from the frontend
   });
 
